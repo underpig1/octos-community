f0725124d26e4a51bcc05976afbf43d26dc9e9e7
underpig1 <chasepgraham@icloud.com>
2025-08-11 01:23:40 -1000

    add globe

--- src/globe/elevation.py
+++ src/globe/elevation.py
@@ -1,35 +1,11 @@
-import os
-import requests
+# script to process globe elevation data into json
 import rasterio
 import json
 import numpy as np
-import zipfile
 
-ETOPO1_URL = 'https://www.ngdc.noaa.gov/mgg/global/relief/ETOPO1/data/bedrock/grid_registered/georeferenced_tiff/ETOPO1_Bed_g_geotiff.zip'
-ZIP_FILE = 'ETOPO1_Bed_g_geotiff.zip'
-EXTRACTED_TIF = 'ETOPO1_Bed_g_geotiff/ETOPO1_Bed_g.tif'  # inside the zip
+EXTRACTED_TIF = '../../../../Downloads/ETOPO1_Bed_g_geotiff/ETOPO1_Bed_g_geotiff.tif'
 
-with open('ETOPO1_Bed_g_geotiff.zip', 'rb') as f:
-    print(f.read(4))
-
-
-def download_etopo1():
-    if os.path.exists(EXTRACTED_TIF):
-        print('Elevation file already downloaded and extracted.')
-        return
-    print('Downloading ETOPO1 GeoTIFF...')
-    r = requests.get(ETOPO1_URL, stream=True)
-    with open(ZIP_FILE, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=8192):
-            f.write(chunk)
-    print('Download complete.')
-    
-    with zipfile.ZipFile(ZIP_FILE, 'r') as zip_ref:
-        zip_ref.extractall()
-    print('Extraction complete.')
-    os.remove(ZIP_FILE)
-
-def elevation_to_globejson(elevation_tif_path, output_json_path, sample_step=100, size_scale=0.005):
+def elevation_to_globejson(elevation_tif_path, output_json_path, sample_step=100, size_scale=0.0002):
     with rasterio.open(elevation_tif_path) as src:
         elevation = src.read(1)
         transform = src.transform
@@ -50,8 +26,7 @@ def elevation_to_globejson(elevation_tif_path, output_json_path, sample_step=100
     print(f'Saved {len(data)//3} points to {output_json_path}')
 
 def main():
-    download_etopo1()
-    elevation_to_globejson(EXTRACTED_TIF, 'globe_elevation.json')
+    elevation_to_globejson(EXTRACTED_TIF, './src/globe/globe_elevation.json')
 
 if __name__ == '__main__':
     main()
